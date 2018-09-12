1e8d1b9ef3f72ea0fd9ea7fa9fbbae5d8c6f79d5
wangyf10 <wangyf10@yusys.com.cn>
2018-09-12 20:25:10 +0800

    初始代码提交

---
+++ src/main/java/com/figue/undertow/demo/fileserving/FileServer.java
@@ -0,0 +1,42 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.figue.undertow.demo.fileserving;
+
+import static io.undertow.Handlers.resource;
+
+import java.nio.file.Paths;
+
+import io.undertow.Undertow;
+import io.undertow.server.handlers.resource.PathResourceManager;
+
+/**
+ * 文件服务器
+ */
+public class FileServer {
+
+    public static void main(final String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(resource(new PathResourceManager(Paths.get("F:\\admin前端\\党费\\admin-web"), 100))
+                        .setDirectoryListingEnabled(true))
+                .build();
+        server.start();
+    }
+
+}
